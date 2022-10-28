ba280767fa1588632e1010976a47c9cd5b322679
TienLee2 <nguyenletien21052002@gmail.com>
2022-10-28 18:50:32 +0700

    First Commit

---
+++ PRN_Project/Models/Account.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace PRN_Project.Models
+{
+    public partial class Account
+    {
+        public Account()
+        {
+            Bills = new HashSet<Bill>();
+        }
+
+        public int Id { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public bool? IsCus { get; set; }
+        public string Email { get; set; }
+        public string Address { get; set; }
+        public string Phone { get; set; }
+        public string CustomerType { get; set; }
+        public int? BonusPoint { get; set; }
+
+        public virtual ICollection<Bill> Bills { get; set; }
+    }
+}
---
+++ PRN_Project/Models/Detail.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace PRN_Project.Models
+{
+    public partial class Detail
+    {
+        public int? BillId { get; set; }
+        public string Price { get; set; }
+        public int? Amount { get; set; }
+        public int? IdProduct { get; set; }
+
+        public virtual Bill Bill { get; set; }
+        public virtual Product IdProductNavigation { get; set; }
+    }
+}
